9399f467bcb845a3208152c9c0826e158abd2848
Pablo Sánchez <49475267+pabsangar@users.noreply.github.com>
2022-04-07 18:37:20 +0200

    Delete app directory

--- app/api/laWifi/datosLinea.js
+++
@@ -1,8 +0,0 @@
-import apiClient from "./client";
-
-const endpoint = "/datoslinea";
-
-const getDatosLinea = (username, linea) =>
-  apiClient.post(endpoint, { username, linea });
-
-export default { getDatosLinea };
